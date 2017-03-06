3c1237958685edc6d2a139dff59ba6a3e005392b
KeiUtsumi <st061660@m01.kyoto-kcg.ac.jp>
2017-03-06 23:12:41 +0900

    day3-7

--- app.js
+++ app.js
@@ -22,18 +22,25 @@ server.listen(port, function() {
   console.log('server running on port', port);
 });
 
-
+var members = [];
 
 const io = socketio.listen(server);
 io.sockets.on('connection', function(socket) {
-  socket.on('message', function(data) {
-    var loginMessage = data.value + "ID :" + socket.id;
+  socket.on('inMessage', function(data) {
+    var loginMessage = data.value + " -> [ID : " + socket.id + "]";
     console.log(loginMessage);
-    io.sockets.emit('from_server', {value: loginMessage});
+    io.sockets.emit('from_server_id', {value: loginMessage});
+  });
+  socket.on('login', function(data) {
+    var commentMessage = " user name : " + data.value + " <font color='#c0c0c0'>[ID : " + socket.id + "]</font> Join!!";
+    console.log(commentMessage);
+    io.sockets.emit('from_server_login', {value: commentMessage});
+    members[socket.id] = data.value;
   });
   socket.on('comment', function(data) {
-    var commentMessage = "ID :" + socket.id + ":" + data.value;
+    var commentMessage = members[socket.id] + " : " + data.value + " <font color='#d0d0d0'>[ID : " + socket.id + "]</font>";
     console.log(commentMessage);
-    socket.broadcast.emit('from_server', {value: commentMessage});
+    //socket.broadcast.emit('from_server_comment', {value: commentMessage});
+    io.sockets.emit('from_server_comment', {value: commentMessage});
   });
 });
